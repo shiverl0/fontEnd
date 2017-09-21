04c81d5cb5f49dd73488f0c28dbc49a701d20145
shiverl0 <lsj950816@qq.com>
2017-09-21 19:40:33 +0800

    ……
    ……

---
+++ JavaScript demo/下拉菜单/JS方法实现下拉菜单/JS/myjs.js
@@ -0,0 +1,8 @@
+function showMenu(li){
+	var show = li.getElementsByTagName("ul")[0];
+	show.style.display = "block";
+}
+function hideMenu(li){
+	var hide = li.getElementsByTagName("ul")[0];
+	hide.style.display = "none";
+}
---
+++ JavaScript demo/方法练习/js/myjs.js
@@ -0,0 +1,4 @@
+var otest = document.getElementById("appChild");
+var newnode = document.createElement("p");
+newnode.innerHTML = "This is a new p";
+otest.appendChild(newnode);
